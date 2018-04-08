dd68079e2af3a9e4fb87c75aa32f64f9948f2a42
Dylan Mahala <dmmahala@hotmail.com>
2018-04-08 14:08:52 -0500

    completed user-signup... probably

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ def index():
 @app.route('/', methods=['POST'])
 def welcome():
     username = request.form['username']
-    password = request.form'[password']
+    password = request.form['password']
     verify = request.form['verify']
     email = request.form['email']
     username_error = ""
@@ -30,23 +30,33 @@ def welcome():
     if password == "":
         password_error = "Please enter a password"
         good_password = False
-    if len(username) < 3 or > 20:
+    if len(username) < 3 or len(username) > 20:
+        username = ""
         username_error = "Username must be between 3 and 20 characters."
         good_username = False
     for character in username:
-        if character in string.punctuation or in string.whitespace:
+        if character in string.punctuation or character in string.whitespace:
             username_error = username + " is not a valid username, please do not use spaces or special characters"
+            username = ""
             good_username = False
-    if password != verify:
-        verify_error = "Password inputs do not match. Please try agian."
+    if not password == verify:
+        verify_error = "Password inputs do not match. Please try again."
         good_verify = False
-    if "@" and "." not in email:
+    if verify == "":
+        verify_error = "Please reenter your password"
+        good_verify = False
+    if "@" not in email and "." not in email:
         email_error = "Please enter a valid email"
+        email = ""
         good_email = False
-    if len(email) < 3 or > 20:
+    if len(email) < 3 or len(email) > 20:
         email_error = "Please enter a valid email"
+        email = ""
         good_email = False
-    
+    if good_username and good_password and good_verify and good_email:
+        return render_template('welcome.html', username=username)
+    else:
+        return render_template("signup-home.html", username=username, email=email, username_error=username_error, password_error=password_error, verify_error=verify_error, email_error=email_error)
     
     
 
